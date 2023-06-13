dd5e47fa5742ab3c09c52b4b16e1e9925a138b3c
Alejandro Velasquez <92229666+VelzckC0D3@users.noreply.github.com>
2023-06-13 17:49:07 -0500

    Update schema_based_on_diagram.sql
    Schema based on the diagram

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -4,9 +4,33 @@ CREATE TABLE patients(
 	date_of_birth DATE
 );
 
-CREATE TABLE medical_histories(
+CREATE TABLE treatments(
 	id SERIAL PRIMARY KEY,
+	type VARCHAR(100),
+	name VARCHAR(100)
+);
+
+CREATE TABLE medical_histories(
+	id SERIAL PRIMARY KEY REFERENCES treatments(id),
 	admitted_at TIMESTAMP,
 	patient_id INT REFERENCES patients(id),
-	status VARCHAR(100)
+	status VARCHAR(100),
+	UNIQUE (id)
+);
+
+CREATE TABLE invoices(
+	id SERIAL PRIMARY KEY,
+	total_amount DECIMAL,
+	generated_at TIMESTAMP,
+	payed_at TIMESTAMP,
+	medical_history_id INT UNIQUE REFERENCES medical_histories(id)
+);
+
+CREATE TABLE invoice_items(
+	id SERIAL PRIMARY KEY,
+	unit_price DECIMAL,
+	quantity INT,
+	total_price DECIMAL,
+	invoice_id INT REFERENCES invoices(id),
+	treatment_id INT REFERENCES treatments(id)
 );
