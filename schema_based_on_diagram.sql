3558a94128f5c6ed0d17c99ccca56fcd8d274840
Juan Sanchez <juansan1993@hotmail.com>
2023-06-13 16:49:44 -0500

    Add 2 tables to schema

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,1 +1,12 @@
+CREATE TABLE patients(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(100),
+	date_of_birth DATE
+);
 
+CREATE TABLE medical_histories(
+	id SERIAL PRIMARY KEY,
+	admitted_at TIMESTAMP,
+	patient_id INT REFERENCES patients(id),
+	status VARCHAR(100)
+);
